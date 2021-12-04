66637b944f2462f40aacf1bec13a43849dc57b39
Taís Vianna <38465611+tvianna@users.noreply.github.com>
2021-12-03 22:26:12 -0300

    build: add swagger module configuration

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const prismaService: PrismaService = app.get(PrismaService);
+
   prismaService.enableShutdownHooks(app);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API Deskbox')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('deskbox')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
